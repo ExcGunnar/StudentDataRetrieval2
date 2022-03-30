2490388c4b65ca88d92378e89511bbdd930e62b1
Gunnar Humphrey <Gunnar_Humphrey@Dell.com>
2022-03-30 14:58:18 -0500

    first commit

---
+++ StudentDataRetrieval2/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.IO;
+using System.Collections;
+
+
+
+string directory = (@"C:\ProjStudentData\");
+
+if (!Directory.Exists(directory))
+{
+    DirectoryInfo dirInf = Directory.CreateDirectory(directory);
+}
+
+Console.WriteLine("Student Directory will be read\n");
+Console.WriteLine($"Directory Location: {directory}\n");
+
+string[] filePaths = Directory.GetFiles(directory, "*.txt");
+
+
+foreach (string filePath in filePaths)
+{
+    string text = File.ReadAllText(filePath);
+    Console.WriteLine($"Filename: {filePath}\n");
+    Console.WriteLine($"{text}\n");
+}
+
